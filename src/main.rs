fa73f70476e5ab025822e465dc0b55797d42918d
Miguel A. Arilla <mangelarilla@users.noreply.github.com>
2022-09-29 23:15:34 +0200

    auto sync once per day

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ mod sync;
 
 extern crate dotenv;
 
+use std::time::Duration;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+
+    tokio::spawn(auto_sync());
 
     let sync = warp::path!("sync")
         .map(|| {
@@ -18,5 +21,15 @@ async fn main() {
     warp::serve(sync)
         .run(([0, 0, 0, 0], 8025))
         .await;
+}
+
+async fn auto_sync() {
+    loop {
+        let join_handle = tokio::task::spawn_blocking(|| sync::run_sync());
+        if let Err(e) = join_handle.await {
+            eprintln!("{:?}", e);
+        }
+        tokio::time::sleep(Duration::from_secs(86400)).await;
+    }
 }
 
