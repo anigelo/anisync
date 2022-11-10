9a7ffd383c4adf4ba063b12083ce0a851b48d4f4
Miguel A. Arilla <mangelarilla@users.noreply.github.com>
2022-11-10 17:13:54 +0100

    test permissions

--- src/sync/sys/unix.rs
+++ src/sync/sys/unix.rs
@@ -6,6 +6,7 @@ pub fn copy_file(from_file: &PathBuf, to_file: &PathBuf) -> Result<()> {
     let operation = fs::copy(from_file, to_file).map(|_| ());
     if operation.is_ok() {
         let mut permissions = to_file.metadata()?.permissions();
+        println!("Parent permissions: {:?}", to_file.parent().unwrap().metadata().unwrap().permissions().mode());
         permissions.set_mode(777);
     }
 }
